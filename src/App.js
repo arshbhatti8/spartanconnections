e07845d2301a316a9bbe043ad052c0e26b5a2ed5
Manpreet Singh <manpreetsingh_er@yahoo.com>
2018-03-06 19:32:53 -0800

    This is a fork and pull request test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Welcome to Spartan Connections
+        Welcome to Spartan Connections and this is a pull request test
       </div>
     );
   }
